5a110be4e9c7dfa5e39bff26a1c2dc9169ed3cf8
manishraj061 <rajputmanish061@gmail.com>
2019-12-05 08:57:07 +0000

    firsty

---
+++ launch_instance.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "myec2server" {
+  provider      = aws
+  ami           = var.amis[var.region]
+  instance_type = var.instance_type
+  availability_zone = var.availability_zone
+  private_ip = var.private_ip
+  key_name = var.key_name
+  tags = local.common_tags
+}
