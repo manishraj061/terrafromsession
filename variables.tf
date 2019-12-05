5bb601aec0783fc1fe2daf2e9169d733e6a1e6d4
manishraj061 <rajputmanish061@gmail.com>
2019-12-05 08:59:04 +0000

    first

---
+++ variables.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  profile    = "default"
+  region     = var.region
+}
+variable "region" {
+  description = "region"
+}
+variable "amis" {
+  type = map
+}
+variable "name" {}
+variable "instance_type" {}
+variable "availability_zone" {description = "Zone in which instance exist"}
+variable "key_name" {}
+variable "private_ip" {}
+variable "description" {}
+variable "bucket_name" {}
+
+locals {
+  Environment = "Development"
+  Owner = "Devops Team"
+}
+
+locals {
+  # Common tags to be assigned to all resources
+  common_tags = {
+    Environment = local.Environment
+    Owner   = local.Owner
+  }
+}
